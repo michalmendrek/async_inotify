6b3887555d90d67dd0c66a6e4362cb1b185e7d62
Michal <michal.mendrek@intive.com>
2019-09-16 14:37:12 +0200

    Formating style

--- notification.cpp
+++ notification.cpp
@@ -1,78 +1,53 @@
-#include <notification.h>
-#include <limits.h>
-#include <sys/inotify.h>
-#include <signal.h>
 #include <fcntl.h>
+#include <limits.h>
+#include <notification.h>
+#include <signal.h>
+#include <sys/inotify.h>
 #include <unistd.h>
+#include <functional>
 #include <iostream>
 #include <string>
-#include <functional>
-
 
 std::function<void(int)> callback_wrapper;
-void callback_function(int sig)
-{
-  callback_wrapper(sig);
-}
+void callback_function(int sig) { callback_wrapper(sig); }
 
-
-
-
-notify::notify(std::string LogDir)
-{
-  std::string File=LogDir;
-  inotifyFD=inotify_init();
-  if(inotifyFD == -1) {}
-  inotifyWD = inotify_add_watch(inotifyFD,File.c_str(),IN_CLOSE_WRITE);
-  if(inotifyWD == -1) {}
+notify::notify(std::string LogDir) {
+  std::string File = LogDir;
+  inotifyFD = inotify_init();
+  if (inotifyFD == -1) {
+  }
+  inotifyWD = inotify_add_watch(inotifyFD, File.c_str(), IN_CLOSE_WRITE);
+  if (inotifyWD == -1) {
+  }
   callback_wrapper = std::bind1st(std::mem_fun(&notify::handler), this);
-  sig_a.sa_handler=callback_function;
+  sig_a.sa_handler = callback_function;
   sigemptyset(&sig_a.sa_mask);
-  sig_a.sa_flags=0;
+  sig_a.sa_flags = 0;
   sigaction(SIGIO, &sig_a, NULL);
-  fcntl(inotifyFD,F_SETFL,O_ASYNC);
-  fcntl(inotifyFD,F_SETOWN,getpid());
-
-
+  fcntl(inotifyFD, F_SETFL, O_ASYNC);
+  fcntl(inotifyFD, F_SETOWN, getpid());
 }
 
+notify::~notify() {}
 
-notify::~notify()
-{
-
-}
-
-
-void notify::handler(int sig)
-{
-
+void notify::handler(int sig) {
   numRead = read(inotifyFD, buff, BUF_LEN);
-  if(numRead > 0){
-
-  for(p = buff; p < buff + numRead;)
-  {
-   event = (struct inotify_event *) p;
-   if(event->len >0){
-   for(auto it:File)
-   {
-    if(event->name  == std::get<0>(it))
-    {
-      //auto ptr = 
-      std::get<1>(it)();
-      //ptr();
-     std::cout << event->name << std::endl;
+  if (numRead > 0) {
+    for (p = buff; p < buff + numRead;) {
+      event = (struct inotify_event *)p;
+      if (event->len > 0) {
+        for (auto it : File) {
+          if (event->name == std::get<0>(it)) {
+            std::get<1>(it)();
+            std::cout << event->name << std::endl;
+          }
+        }
+      }
+      p += sizeof(struct inotify_event) + event->len;
     }
-   }
-   }
-   p+= sizeof(struct inotify_event) + event->len;
   }
-  }
-
-
 }
 
-
-void notify::AddFileName(std::string FileName, std::function<void()> func)
-{
- File.push_back(std::make_tuple(FileName,func));
+void notify::AddFileName(std::string FileName, std::function<void()> func) {
+  File.push_back(std::make_tuple(FileName, func));
 }
--- notification.h
+++ notification.h
@@ -1,24 +1,21 @@
 #ifndef NOTIFICATION
 #define NOTIFICATION
-#include <iostream>
-#include <string>
 #include <limits.h>
 #include <signal.h>
-#include <functional>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/inotify.h>
-#include <vector>
+#include <unistd.h>
+#include <functional>
+#include <iostream>
+#include <string>
 #include <tuple>
+#include <vector>
 
-#define BUF_LEN (10 * (sizeof (struct inotify_event) + NAME_MAX +1))
+#define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
 
-class notify
-{
-
-private:
-
+class notify {
+ private:
   std::vector<std::tuple<std::string, std::function<void()>>> File;
 
   int inotifyFD;
@@ -27,16 +24,14 @@ private:
   std::function<void(int)> callback;
   char buff[BUF_LEN];
   char *p;
-  struct  inotify_event *event;
+  struct inotify_event *event;
   ssize_t numRead;
   void handler(int sig);
-public:
+
+ public:
   notify(std::string LogDir);
- ~notify();
+  ~notify();
   void AddFileName(std::string FileName, std::function<void()> func);
-
-
 };
-
 
 #endif
